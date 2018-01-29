127ae2e505a0f338a86c4b22a9f9055b3296ab88
Diego Morales <dimofra9@gmail.com>
2018-01-29 08:37:22 -0500

    paso 6
    incluir otras fotos

--- searcher.php
+++ searcher.php
@@ -1,5 +1,5 @@
 <?php
-//Archivo php para llada AJAX
+//Archivo php para llamada AJAX. 
 $ciudad = htmlspecialchars($_GET['ciudad']);
 $tipo = htmlspecialchars($_GET['tipo']);
 $precio = htmlspecialchars($_GET['precio']);
@@ -7,52 +7,51 @@ $precio = htmlspecialchars($_GET['precio']);
 $pos = strpos($precio, ';');
 
 $min = substr($precio, 0, $pos);
-$max = substr($precio, $pos + 1);
+$max = substr($precio, $pos+1);
 
-$file = fopen('data-1.json', 'r') or die ('No se puede abrir el archivo');
+$file = fopen("data-1.json", "r") or die("No se puede abrir el archivo");
 
 $json = fread($file, filesize('data-1.json'));
 $data = json_decode($json, true);
 
-//filtros
+//primer filtro de precios
 $r = array();
 foreach($data as $i){
-	$p = $i['Precio'];
-	$p = substr($p, strpos($p,'$') + 1);
-	$c = strpos($p, ',');
-	$p = substr($p, 0, $c).substr($, $c + 1);
-	if($p>=$min && $p<=$max){
-		array_push($r, $i);
-	}
+    $p = $i['Precio'];
+    $p = substr($p, strpos($p,'$')+1);
+    $c = strpos($p,',');
+    $p = substr($p,0,$c).substr($p,$c+1);
+    if($p>=$min && $p<=$max){
+        array_push($r, $i);
+    }
 }
 
-$resul = array();
+$result = array();
 if(!empty($ciudad) && !empty($tipo)){
-	foreach($r as $e){
-		if($e['Ciudad']==$ciudad && $e['Tipo']==$tipo){
-			array_push($resul, $e);
-		}
-	}
+    foreach($r as $e){
+        if($e['Ciudad']==$ciudad && $e['Tipo']==$tipo){
+            array_push($result, $e);
+        }
+    }
 } elseif(!empty($ciudad)){
-	foreach($r as $e){
-		if($e['Ciudad']==$ciudad){
-			array_push($resul, $e);
-		}
-	}
+    foreach($r as $e){
+        if($e['Ciudad']==$ciudad){
+            array_push($result, $e);
+        }
+    }
 } elseif(!empty($tipo)){
-	foreach($r as $e){
-		if($e[´Tipo]==$ciudad){
-			array_push($resul, $e);
-		}
-	}
+    foreach($r as $e){
+        if($e['Tipo']==$tipo){
+            array_push($result, $e);
+        }
+    }
 } else {
-	$resul = $r;
+    $result = $r;
 }
 
 
-$rjson = json_encode($resul);
-echo('{"resul";"success", "message":"Resultados obtenidos exitosamente", "data":'.$rjson'}');
+$rjson = json_encode($result);
+echo '{"result":"success", "message":"Resultados obtenidos exitosamente", "data":'.$rjson.'}';
 
 fclose($file);
-
 ?>
